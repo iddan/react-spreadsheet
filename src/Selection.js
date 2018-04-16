d344fc5c2e8554b1005b4d98ac5fa5428ffe30bc
Iddan Aharonson <mail@iddan.co>
2018-04-16 13:53:15 +0300

    removed selection module

--- src/Selection.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import React from "react";
-import FloatingRect from "./FloatingRect";
-import "./Selection.css";
-
-const Selection = () => <FloatingRect set="selected" className="Selection" />;
-
-export default Selection;
