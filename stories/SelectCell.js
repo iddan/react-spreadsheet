946c8ef4ac5a281bc941f1dc0f9595546df90967
Iddan Aaronsohn <mail@aniddan.com>
2018-06-21 18:07:24 +0300

    Select example

--- stories/SelectCell.js
+++ stories/SelectCell.js
@@ -4,26 +4,38 @@ import Spreadsheet from "../src/SpreadsheetStateProvider";
 import { range } from "../src/util";
 import { INITIAL_ROWS, INITIAL_COLUMNS } from "./Basic";
 import Select from "react-select";
+import "react-select/dist/react-select.css";
 import "./index.css";
 
 const initialData = range(INITIAL_ROWS).map(() => Array(INITIAL_COLUMNS));
 
-const OPTIONS = ["Vanilla", "Chocolate", "Caramel"];
+const OPTIONS = [
+  { value: "vanilla", label: "Vanilla" },
+  { value: "chocolate", label: "Chocolate" },
+  { value: "caramel", label: "Caramel" }
+];
 
 const SelectView = ({ cell, getValue }) => (
-  <Select value={getValue({ data: cell })} options={OPTIONS} disabled />
+  <Select value={getValue({ data: cell })} options={OPTIONS} disabled style={{ width: 200 }} />
 );
 
 class SelectEdit extends Component {
   handleChange = selection => {
     const { onChange, cell } = this.props;
-    onChange({ ...cell, value: selection.value });
+    onChange({ ...cell, value: selection ? selection.value : null });
   };
 
   render() {
     const { getValue, column, row, cell } = this.props;
     const value = getValue({ column, row, data: cell }) || 0;
-    return <Select value={value} onChange={this.handleChange} />;
+    return (
+      <Select
+        value={value}
+        onChange={this.handleChange}
+        options={OPTIONS}
+        style={{ width: 200 }}
+      />
+    );
   }
 }
 
--- stories/index.js
+++ stories/index.js
@@ -4,10 +4,12 @@ import Spreadsheet from "../src/SpreadsheetStateProvider";
 import Basic from "./Basic";
 import Controlled from "./Controlled";
 import CustomCell from "./CustomCell";
+import SelectCell from "./SelectCell";
 import Filter from "./Filter";
 
 storiesOf("Spreadsheet", Spreadsheet)
   .add("Basic", () => <Basic />)
   .add("Controlled", () => <Controlled />)
   .add("CustomCell", () => <CustomCell />)
-  .add("Filter", () => <Filter />);
+  .add("Filter", () => <Filter />)
+  .add("Select Cell", () => <SelectCell />);
