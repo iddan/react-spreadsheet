8c038d591dedeffc6fc72049e1ccad5fc256d703
Iddan Aaronsohn <mail@aniddan.com>
2021-08-20 14:58:05 +0000

    Update storybook to use Webpack 5 builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
 };
