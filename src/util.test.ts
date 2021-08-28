afca00b01c362d187c33c1f1bf84437a5ee86d0a
Iddan Aaronsohn <mail@aniddan.com>
2021-08-28 17:48:44 +0000

    Test getCellDimensions

--- src/util.test.ts
+++ src/util.test.ts
@@ -3,17 +3,59 @@ import {
   calculateSpreadsheetSize,
   createEmptyMatrix,
   range,
+  getCellDimensions,
 } from "./util";
+import * as types from "./types";
+import * as PointMap from "./point-map";
 
 const EXAMPLE_INPUT_VALUE = "EXAMPLE_INPUT_VALUE";
 const EXAMPLE_DATA_ROWS_COUNT = 2;
 const EXAMPLE_DATA_COLUMNS_COUNT = 2;
-const EXAMPLE_DATA = createEmptyMatrix(
+const EXAMPLE_DATA = createEmptyMatrix<types.CellBase>(
   EXAMPLE_DATA_ROWS_COUNT,
   EXAMPLE_DATA_COLUMNS_COUNT
 );
 const EXAMPLE_ROW_LABELS = ["Foo", "Bar", "Baz"];
 const EXAMPLE_COLUMN_LABELS = ["Foo", "Bar", "Baz"];
+const EXAMPLE_EXISTING_POINT: types.Point = {
+  row: 0,
+  column: 0,
+};
+const EXAMPLE_NON_EXISTING_POINT: types.Point = {
+  row: EXAMPLE_DATA_ROWS_COUNT,
+  column: EXAMPLE_DATA_COLUMNS_COUNT,
+};
+const EXAMPLE_CELL_DIMENSIONS: types.Dimensions = {
+  height: 200,
+  width: 20,
+  top: 0,
+  left: 0,
+};
+const EXAMPLE_STATE: types.StoreState = {
+  active: null,
+  mode: "view",
+  rowDimensions: {
+    [EXAMPLE_EXISTING_POINT.row]: {
+      height: EXAMPLE_CELL_DIMENSIONS.height,
+      top: EXAMPLE_CELL_DIMENSIONS.top,
+    },
+  },
+  columnDimensions: {
+    [EXAMPLE_EXISTING_POINT.column]: {
+      width: EXAMPLE_CELL_DIMENSIONS.width,
+      left: EXAMPLE_CELL_DIMENSIONS.left,
+    },
+  },
+  lastChanged: null,
+  hasPasted: false,
+  cut: false,
+  dragging: false,
+  data: EXAMPLE_DATA,
+  selected: null,
+  copied: PointMap.from([]),
+  bindings: PointMap.from([]),
+  lastCommit: null,
+};
 
 describe("moveCursorToEnd()", () => {
   test("moves cursor to the end", () => {
@@ -78,5 +120,18 @@ describe("calculateSpreadsheetSize()", () => {
       rows: EXAMPLE_DATA_ROWS_COUNT,
       columns: EXAMPLE_COLUMN_LABELS.length,
     });
+  });
+});
+
+describe("getCellDimensions", () => {
+  test("returns existing cell dimensions", () => {
+    expect(getCellDimensions(EXAMPLE_EXISTING_POINT, EXAMPLE_STATE)).toEqual(
+      EXAMPLE_CELL_DIMENSIONS
+    );
+  });
+  test("returns null for non existing cell", () => {
+    expect(
+      getCellDimensions(EXAMPLE_NON_EXISTING_POINT, EXAMPLE_STATE)
+    ).toBeUndefined();
   });
 });
