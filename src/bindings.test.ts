d42318628a5705a6eb6c1509c5c5cdcefe13695a
Iddan Aaronsohn <mail@aniddan.com>
2021-09-02 10:08:45 +0000

    Improve types of bindings test

--- src/bindings.test.ts
+++ src/bindings.test.ts
@@ -1,22 +1,21 @@
-import {
-  getBindingsForCell,
-} from "./bindings";
+import { getBindingsForCell } from "./bindings";
 import { Matrix } from "./matrix";
+import * as Point from "./point";
 import { CellBase } from "./types";
 
 const A1 = "A1";
 const A2 = "A2";
-const A1_POINT = { row: 0, column: 0 };
-const A2_POINT = { row: 1, column: 0 };
 const B1 = "B1";
-const B1_POINT = { row: 0, column: 1 };
 const B2 = "B2";
-const B2_POINT = { row: 1, column: 1 };
+const A1_POINT: Point.Point = { row: 0, column: 0 };
+const A2_POINT: Point.Point = { row: 1, column: 0 };
+const B1_POINT: Point.Point = { row: 0, column: 1 };
+const B2_POINT: Point.Point = { row: 1, column: 1 };
 const SUM_A1_A2_FORMULA = `=SUM(${A1}, ${A2})`;
 const SUM_B1_B2_FORMULA = `=SUM(${B1}, ${B2})`;
 const EXAMPLE_FORMULA_CELL = { value: SUM_A1_A2_FORMULA };
 const EMPTY_DATA: Matrix<CellBase> = [];
-const EXAMPLE_DATA = [
+const EXAMPLE_DATA: Matrix<CellBase> = [
   [{ value: 1 }, { value: 2 }],
   [{ value: SUM_B1_B2_FORMULA }, { value: 3 }],
 ];
