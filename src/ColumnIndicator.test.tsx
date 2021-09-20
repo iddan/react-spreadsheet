1242cb47b58ebbb8f667289dfb3d98943a17e059
Iddan Aaronsohn <mail@aniddan.com>
2021-09-20 21:39:33 +0000

    Update column indicator tests

--- src/ColumnIndicator.test.tsx
+++ src/ColumnIndicator.test.tsx
@@ -4,16 +4,22 @@
 
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import ColumnIndicator from "./ColumnIndicator";
+import ColumnIndicator, { Props } from "./ColumnIndicator";
+
+const EXAMPLE_PROPS: Props = {
+  column: 0,
+  selected: false,
+  onSelect: jest.fn(),
+};
 
 describe("<ColumnIndicator />", () => {
   test("renders with column letter", () => {
-    render(<ColumnIndicator column={0} />);
+    render(<ColumnIndicator {...EXAMPLE_PROPS} />);
     expect(document.querySelectorAll("th.Spreadsheet__header").length).toBe(1);
     expect(screen.queryByText("A")).not.toBeNull();
   });
   test("renders with label", () => {
-    render(<ColumnIndicator column={0} label="Example Label" />);
+    render(<ColumnIndicator {...EXAMPLE_PROPS} label="Example Label" />);
     expect(document.querySelectorAll("th.Spreadsheet__header").length).toBe(1);
     expect(screen.queryByText("Example Label")).not.toBeNull();
   });
