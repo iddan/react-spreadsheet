06aeee16209802328f0b8e8eaabe6885d59f0da6
Iddan Aaronsohn <mail@aniddan.com>
2021-09-20 21:42:35 +0000

    Fix CornerIndicator test

--- src/CornerIndicator.test.tsx
+++ src/CornerIndicator.test.tsx
@@ -4,11 +4,16 @@
 
 import React from "react";
 import { render } from "@testing-library/react";
-import CornerIndicator from "./CornerIndicator";
+import CornerIndicator, { Props } from "./CornerIndicator";
+
+const EXAMPLE_PROPS: Props = {
+  selected: false,
+  onSelect: jest.fn(),
+}
 
 describe("<CornerIndicator />", () => {
   test("renders", () => {
-    render(<CornerIndicator />);
+    render(<CornerIndicator {...EXAMPLE_PROPS} />);
     expect(document.querySelectorAll("th.Spreadsheet__header").length).toBe(1);
   });
 });
