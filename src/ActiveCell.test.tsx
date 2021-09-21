3e98c8c0c59994ed8f110e048bd14fc4ecfbda72
Iddan Aaronsohn <mail@aniddan.com>
2021-09-21 17:53:54 +0000

    Test view and edit mode of active cell

--- src/ActiveCell.test.tsx
+++ src/ActiveCell.test.tsx
@@ -4,16 +4,82 @@
 
 import * as React from "react";
 import { render } from "@testing-library/react";
-import ActiveCell from "./ActiveCell";
+import * as Point from "./point";
+import * as Types from "./types";
 import context from "./context";
 import { INITIAL_STATE } from "./reducer";
+import ActiveCell from "./ActiveCell";
+
+const MOCK_DATA_EDITOR = jest.fn(() => null);
+const DISPATCH_MOCK = jest.fn();
+const GET_BINDINGS_FOR_CELL_MOCK = jest.fn();
+const STATE_WITH_ACTIVE: Types.StoreState = {
+  ...INITIAL_STATE,
+  active: Point.ORIGIN,
+  rowDimensions: {
+    0: {
+      height: 42,
+      top: 42,
+    },
+  },
+  columnDimensions: {
+    0: {
+      width: 42,
+      left: 42,
+    },
+  },
+};
 
 describe("<ActiveCell />", () => {
-  test("renders", () => {
+  test("renders hidden when active is not defined", () => {
     render(
-      <context.Provider value={[INITIAL_STATE, jest.fn()]}>
-        <ActiveCell DataEditor={jest.fn()} getBindingsForCell={jest.fn()} />
+      <context.Provider value={[INITIAL_STATE, DISPATCH_MOCK]}>
+        <ActiveCell
+          DataEditor={MOCK_DATA_EDITOR}
+          getBindingsForCell={GET_BINDINGS_FOR_CELL_MOCK}
+        />
       </context.Provider>
+    );
+    expect(document.querySelector(".Spreadsheet__active-cell")).toBeNull();
+    expect(MOCK_DATA_EDITOR).toBeCalledTimes(0);
+  });
+  test("renders when active is defined", () => {
+    render(
+      <context.Provider value={[STATE_WITH_ACTIVE, DISPATCH_MOCK]}>
+        <ActiveCell
+          DataEditor={MOCK_DATA_EDITOR}
+          getBindingsForCell={GET_BINDINGS_FOR_CELL_MOCK}
+        />
+      </context.Provider>
+    );
+    const activeCell = document.querySelector(".Spreadsheet__active-cell");
+    expect(activeCell).not.toBeNull();
+    expect(activeCell).toHaveClass("Spreadsheet__active-cell--view");
+    expect(MOCK_DATA_EDITOR).toBeCalledTimes(0);
+  });
+  test("renders in edit mode", () => {
+    render(
+      <context.Provider
+        value={[{ ...STATE_WITH_ACTIVE, mode: "edit" }, DISPATCH_MOCK]}
+      >
+        <ActiveCell
+          DataEditor={MOCK_DATA_EDITOR}
+          getBindingsForCell={GET_BINDINGS_FOR_CELL_MOCK}
+        />
+      </context.Provider>
+    );
+    const activeCell = document.querySelector(".Spreadsheet__active-cell");
+    expect(activeCell).not.toBeNull();
+    expect(activeCell).toHaveClass("Spreadsheet__active-cell--edit");
+    expect(MOCK_DATA_EDITOR).toBeCalledTimes(1);
+    expect(MOCK_DATA_EDITOR).toBeCalledWith(
+      {
+        row: Point.ORIGIN.row,
+        column: Point.ORIGIN.column,
+        cell: undefined,
+        onChange: expect.any(Function),
+      },
+      {}
     );
   });
 });
