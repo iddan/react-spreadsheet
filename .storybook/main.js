d4bd3e6c6b99cd5e6b6b178934259682d778ea8a
Iddan Aaronsohn <mail@aniddan.com>
2023-04-24 01:36:55 +0300

    Upgrade storybook

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ["../src/**/*.stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  core: {
-    builder: "webpack5",
-  },
-  framework: "@storybook/react",
-};
