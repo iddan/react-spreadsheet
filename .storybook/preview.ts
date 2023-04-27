e2ba648f5a35c1e6a54bcbbde2d9a8c7efafd355
Iddan Aaronsohn <mail@aniddan.com>
2023-04-28 00:18:46 +0300

    Match storybook preview with theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,18 @@
 import type { Preview } from "@storybook/react";
 
+// function to get user dark mode
+const getDarkMode = () => {
+  if (typeof window !== "undefined") {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+  return false;
+};
+
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: getDarkMode() ? "dark" : "light",
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
