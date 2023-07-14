bea779a2f71bd9c9aa83cab6261d462de9b7abb0
Iddan Aaronsohn <mail@aniddan.com>
2023-07-14 03:26:58 +0200

    Remove Point.is

--- src/point.test.ts
+++ src/point.test.ts
@@ -4,16 +4,6 @@ const EXAMPLE_POINT: Point.Point = { row: 0, column: 0 };
 const EXAMPLE_POINT_COPY: Point.Point = { row: 0, column: 0 };
 const EXAMPLE_ANOTHER_POINT: Point.Point = { row: 1, column: 1 };
 
-describe("is()", () => {
-  const cases = [
-    ["A point", EXAMPLE_POINT, true],
-    ["Not a point", null, false],
-  ] as const;
-  test.each(cases)("%s", (name, value, expected) => {
-    expect(Point.is(value)).toBe(expected);
-  });
-});
-
 describe("isEqual()", () => {
   const cases = [
     ["returns true for equal points", EXAMPLE_POINT, EXAMPLE_POINT_COPY, true],
