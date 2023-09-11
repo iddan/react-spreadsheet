38f98a3786e0f90590e10e8da4edc105c8d9aa2c
Iddan Aaronsohn <mail@aniddan.com>
2023-09-11 14:11:33 +0300

    Improve docs and interface

--- src/engine/point-set.ts
+++ src/engine/point-set.ts
@@ -1,15 +1,19 @@
-/**
- * Immutable Set like interface of points
- */
-
 import * as Point from "../point";
 import * as pointHash from "./point-hash";
 
+/**
+ * Immutable Set like interface of points
+ */
 export class PointSet {
   private constructor(private set: Set<string>) {}
+
   /** Creates a new PointSet instance from an array-like or iterable object */
-  static from(points: Point.Point[]): PointSet {
-    return new PointSet(new Set(points.map(pointHash.toString)));
+  static from(points: Iterable<Point.Point>): PointSet {
+    const set = new Set<string>();
+    for (const point of points) {
+      set.add(pointHash.toString(point));
+    }
+    return new PointSet(set);
   }
 
   /** Returns a boolean asserting whether an point is present with the given value in the Set object or not */
@@ -29,7 +33,7 @@ export class PointSet {
     return new PointSet(newSet);
   }
 
-  /** Remove the given point to given set */
+  /** Remove the given point from the given set */
   delete(point: Point.Point): PointSet {
     const newSet = new Set(this.set);
     if (!newSet.delete(pointHash.toString(point))) {
@@ -38,6 +42,7 @@ export class PointSet {
     return new PointSet(newSet);
   }
 
+  /** Returns a new PointSet with points common to the set and other */
   difference(other: PointSet): PointSet {
     let newSet = this as PointSet;
     for (const point of other) {
@@ -46,6 +51,7 @@ export class PointSet {
     return newSet;
   }
 
+  /** Returns a new PointSet with all points in both sets */
   union(other: PointSet): PointSet {
     let newSet = this as PointSet;
     for (const point of other) {
@@ -54,6 +60,7 @@ export class PointSet {
     return newSet;
   }
 
+  /** Creates an iterator of points in the set */
   *[Symbol.iterator](): Iterator<Point.Point> {
     for (const value of this.set) {
       yield pointHash.fromString(value);
