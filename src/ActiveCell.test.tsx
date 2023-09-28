684345e28a2f3048072836f142799e77431bef73
Iddan Aaronsohn <mail@aniddan.com>
2023-09-28 23:40:22 +0300

    Add more tests to active cell

--- src/ActiveCell.test.tsx
+++ src/ActiveCell.test.tsx
@@ -3,12 +3,14 @@
  */
 
 import * as React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import * as Point from "./point";
 import * as Types from "./types";
+import * as Actions from "./actions";
 import context from "./context";
 import { INITIAL_STATE } from "./reducer";
 import ActiveCell from "./ActiveCell";
+import DataEditor from "./DataEditor";
 
 const MOCK_DATA_EDITOR = jest.fn(() => null);
 const DISPATCH_MOCK = jest.fn();
@@ -30,6 +32,9 @@ const STATE_WITH_ACTIVE: Types.StoreState = {
 };
 
 describe("<ActiveCell />", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   test("renders hidden when active is not defined", () => {
     render(
       <context.Provider value={[INITIAL_STATE, DISPATCH_MOCK]}>
@@ -72,5 +77,55 @@ describe("<ActiveCell />", () => {
       },
       {}
     );
+  });
+  test("calls setCellData if value changed", () => {
+    const { rerender } = render(
+      <context.Provider
+        value={[{ ...STATE_WITH_ACTIVE, mode: "edit" }, DISPATCH_MOCK]}
+      >
+        <ActiveCell DataEditor={DataEditor} />
+      </context.Provider>
+    );
+    const activeCell = document.querySelector(".Spreadsheet__active-cell");
+    expect(activeCell).not.toBeNull();
+    expect(activeCell).toHaveClass("Spreadsheet__active-cell--edit");
+    const input = activeCell?.querySelector("input");
+    if (!input) throw new Error("input not found");
+    fireEvent.change(input, { target: { value: "test" } });
+    expect(DISPATCH_MOCK).toBeCalledTimes(1);
+    expect(DISPATCH_MOCK).toBeCalledWith(
+      Actions.setCellData(Point.ORIGIN, {
+        value: "test",
+      })
+    );
+    rerender(
+      <context.Provider
+        value={[{ ...STATE_WITH_ACTIVE, mode: "view" }, DISPATCH_MOCK]}
+      >
+        <ActiveCell DataEditor={DataEditor} />
+      </context.Provider>
+    );
+    expect(activeCell).not.toHaveClass("Spreadsheet__active-cell--edit");
+  });
+  test("doesn't call setCellData if value not changed", () => {
+    const { rerender } = render(
+      <context.Provider
+        value={[{ ...STATE_WITH_ACTIVE, mode: "edit" }, DISPATCH_MOCK]}
+      >
+        <ActiveCell DataEditor={MOCK_DATA_EDITOR} />
+      </context.Provider>
+    );
+    const activeCell = document.querySelector(".Spreadsheet__active-cell");
+    expect(activeCell).not.toBeNull();
+    expect(activeCell).toHaveClass("Spreadsheet__active-cell--edit");
+    rerender(
+      <context.Provider
+        value={[{ ...STATE_WITH_ACTIVE, mode: "view" }, DISPATCH_MOCK]}
+      >
+        <ActiveCell DataEditor={MOCK_DATA_EDITOR} />
+      </context.Provider>
+    );
+    expect(DISPATCH_MOCK).toBeCalledTimes(0);
+    expect(activeCell).not.toHaveClass("Spreadsheet__active-cell--edit");
   });
 });
